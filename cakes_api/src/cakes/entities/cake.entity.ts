55497252321fa73f0a949a77171e55d160aa056c
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2022-08-22 15:52:51 -0300

    create entities

--- cakes_api/src/cakes/entities/cake.entity.ts
+++ cakes_api/src/cakes/entities/cake.entity.ts
@@ -1,8 +1,19 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('cakes')
 export class Cake {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   forUntil: string;
+
+  @Column()
   image: string;
 }
--- cakes_api/src/main.ts
+++ cakes_api/src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(3000, () => console.log('Server running on port 3000'));
 }
 bootstrap();
